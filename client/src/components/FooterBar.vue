5220d6875935c8490a526f5ff5ea64df39c06bb3
Rudy <rudyalvs@gmail.com>
2025-12-08 18:06:14 +0100

    update: version

--- client/src/components/FooterBar.vue
+++ client/src/components/FooterBar.vue
@@ -2,7 +2,7 @@
     <footer class="footer">
         <div class="left">{{ timeZone }} (UTC{{ utcOffset }})</div>
         <div class="center">Développé par {{ developerName }}</div>
-        <div class="right">© {{ currentYear }} AbView v1.0</div>
+        <div class="right">© {{ currentYear }} AbView v1.3</div>
     </footer>
 </template>
 
